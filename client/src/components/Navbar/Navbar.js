da77a812673678740d168cc44224ebfad1cd83e4
Fadlan Fadilah <fhadlan123@gmail.com>
2023-09-07 10:10:46 +0700

    fixing ui

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { AppBar, Link, Toolbar, Typography } from "@mui/material";
+import { AppBar, Avatar, Box, Link, Toolbar, Typography } from "@mui/material";
 import logo from "../../images/memories-Logo.png";
 
 const Navbar = () => {
+  const user = true;
   return (
     <>
       <AppBar position="static" color="default" variant="outlined">
-        <Toolbar>
-          <Typography component={Link} href="/" variant="h2">
+        <Toolbar sx={{ display: "flex" }}>
+          <img src={logo} alt="memories" height="48rem" width={"48rem"} />
+          <Typography
+            component={Link}
+            href="/"
+            variant="h2"
+            display={{ xs: "none", sm: "inline" }}
+          >
             SosialGram
           </Typography>
-          <img src={logo} alt="memories" height="48rem" width={"48rem"} />
+          <Box flexGrow={1} />
+          {user ? <Avatar></Avatar> : <Typography>belum login</Typography>}
         </Toolbar>
       </AppBar>
     </>
