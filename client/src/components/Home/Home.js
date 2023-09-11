02a34277c4aedfee5b72e40f82bb5f84c904590f
Fadlan Fadilah <fhadlan123@gmail.com>
2023-09-11 08:15:58 +0700

    fixing route, add home & auth component

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Grow, Grid } from "@mui/material";
+import Posts from "../Posts/Posts";
+import PostForm from "../Form/PostForm";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Grow in>
+          <Grid container justifyContent="space-between" mt={1} spacing={1}>
+            <Grid item xs={12} sm={8} md={9}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+              <PostForm />
+            </Grid>
+          </Grid>
+        </Grow>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
